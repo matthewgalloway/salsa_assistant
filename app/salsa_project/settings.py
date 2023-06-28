76e118c036a98c08155336e8f2044c95f3076d9f
mgalloway <matthew.galloway19@gmail.com>
2023-06-28 09:34:42 +0100

    updating media and static URLs

--- app/salsa_project/settings.py
+++ app/salsa_project/settings.py
@@ -125,8 +125,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+
+STATIC_URL = '/static/static/'
+MEDIA_URL = '/static/media/'
+MEDIA_ROOT = 'vol/web/media'
+STATIC_ROOT = 'vol/web/static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
